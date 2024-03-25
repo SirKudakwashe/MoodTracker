de245a7208ef2add2ab340ed773fc0e81bfa54ba
Kudakwashe Matambura <117982343+SirKudakwashe@users.noreply.github.com>
2024-03-25 15:31:36 +0200

    Persisting Data using async storage

--- src/App.provider.tsx
+++ src/App.provider.tsx
@@ -1,5 +1,28 @@
-import React, { useContext, useCallback, useState } from 'react';
+import React, { useContext, useCallback, useState, useEffect } from 'react';
 import { MoodOptionWithTimestamp, MoodOptionType } from './types';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+type AppData = {
+  moodList: MoodOptionWithTimestamp[];
+};
+
+const dataKey = 'my-app-data';
+
+const setAppData = async (appData: AppData): Promise<void> => {
+  try {
+    const result = await AsyncStorage.setItem(dataKey, JSON.stringify(appData));
+  } catch {}
+};
+
+const getAppData = async (): Promise<AppData | undefined> => {
+  try {
+    const result = await AsyncStorage.getItem(dataKey);
+    if (result) {
+      return JSON.parse(result);
+    }
+  } catch {}
+  return undefined;
+};
 
 type AppContextType = {
   moodList: MoodOptionWithTimestamp[];
@@ -19,8 +42,24 @@ const AppContext = React.createContext<AppContextType>(defaultValue);
 export const AppProvider: React.FC<Props> = ({ children }) => {
   const [moodList, setMoodList] = useState<MoodOptionWithTimestamp[]>([]);
   const handleSelectMood = useCallback((mood: MoodOptionType) => {
-    setMoodList(current => [...current, { mood, timestamp: Date.now() }]);
+    setMoodList(current => {
+      const newMoodList = [...current, { mood, timestamp: Date.now() }];
+      setAppData({ moodList: newMoodList });
+      return newMoodList;
+    });
   }, []);
+
+  useEffect(() => {
+    const fetchAppData = async () => {
+      const data = await getAppData();
+      if (data) {
+        setMoodList(data.moodList);
+      }
+    };
+
+    fetchAppData();
+  }, []);
+
   return (
     <AppContext.Provider value={{ moodList, handleSelectMood }}>
       {children}
--- src/screens/Home.screen.tsx
+++ src/screens/Home.screen.tsx
@@ -1,12 +1,10 @@
 import React, { useCallback, useState } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 import { MoodPicker } from '../components/MoodPicker';
-import { MoodOptionWithTimestamp, MoodOptionType } from '../types';
-import { MoodItemRow } from '../components/MoodItemRow';
 import { useAppContext } from '../App.provider';
-const appContext = useAppContext();
 
 export const Home: React.FC = () => {
+  const appContext = useAppContext();
   return (
     <View style={styles.container}>
       <MoodPicker handleSelectMood={appContext.handleSelectMood} />
